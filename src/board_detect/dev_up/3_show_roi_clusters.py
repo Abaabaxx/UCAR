5f8d198957acb2768f883602ca57291aa5e7e35c
LBY <agt2643194651@outlook.com>
2025-07-22 21:54:30 +0800

    修改上平面找板逻辑：找板点后移
    修改显示ROI区域与ROI聚类的脚本(下移ROI区域)
    修改amcl_board_up.launch，初始姿态下移
    初始位姿 x=1.25m y=3.5m 角度90°-->初始位姿 x=1.25m y=3.0m 角度90°

--- src/board_detect/dev_up/3_show_roi_clusters.py
+++ src/board_detect/dev_up/3_show_roi_clusters.py
@@ -58,11 +58,11 @@ class ClusterVisualizer:
         self.max_length = 0.60  # 最大长度（米）
         
         # 新增：定义ROI多边形区域
-        self.polygon = Polygon([(-0.2, 3.5), (2.7, 3.5), (2.7, 5.2), (-0.2, 5.2)])
+        self.polygon = Polygon([(-0.2, 3), (2.7, 3), (2.7, 5.2), (-0.2, 5.2)])
         
         rospy.loginfo("ClusterVisualizer initialized, waiting for scan data...")
         rospy.loginfo("Filtering clusters with length between %.2fm and %.2fm", self.min_length, self.max_length)
-        rospy.loginfo("ROI Polygon defined for filtering: [(-0.2, 3.5), (2.7, 3.5), (2.7, 5.2), (-0.2, 5.2)]")
+        rospy.loginfo("ROI Polygon defined for filtering: [(-0.2, 3.0), (2.7, 3.0), (2.7, 5.2), (-0.2, 5.2)]")
         
         # 新增：在初始化后立即发布ROI多边形用于可视化
         self.publish_roi_polygon()
@@ -89,7 +89,7 @@ class ClusterVisualizer:
         marker.color.a = 0.8   # 半透明
         
         # 添加多边形的顶点，注意要闭合
-        vertices = [(-0.2, 3.5), (2.7, 3.5), (2.7, 5.2), (-0.2, 5.2), (-0.2, 3.5)]
+        vertices = [(-0.2, 3), (2.7, 3), (2.7, 5.2), (-0.2, 5.2), (-0.2, 3)]
         for vertex in vertices:
             p = self.create_point(vertex[0], vertex[1], 0.05)  # 稍微抬高一点，避免被地图覆盖
             marker.points.append(p)
