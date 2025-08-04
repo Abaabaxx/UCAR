bb8db54890e7e964f8ce3bbe070a62df330f1bef
abaabaxxx <agt2643194651@gmail.com>
2025-08-05 02:19:41 +0800

    rosbrige：升级了测试，使用英文收发

--- src/ucar2pc/scripts/start_simulation.py
+++ src/ucar2pc/scripts/start_simulation.py
@@ -6,8 +6,7 @@
 import rospy
 import random
 import sys
-# 导入我们刚刚定义的服务类型
-# 请确保您的包名是 ucar2pc，如果不是请修改
+# 导入我们定义的服务类型
 from ucar2pc.srv import FindItem, FindItemRequest
 
 def call_simulation_service():
@@ -15,34 +14,33 @@ def call_simulation_service():
     service_name = '/find_item_simulation'
     
     rospy.loginfo('[小车端] 正在等待名为 \'%s\' 的服务启动...' % service_name)
-    
-    # rospy.wait_for_service 会阻塞程序，直到服务可用，非常重要！
     rospy.wait_for_service(service_name)
     
     try:
-        # 创建一个服务的句柄，就像一个可以直接调用的函数
         find_item_service = rospy.ServiceProxy(service_name, FindItem)
         
-        # 随机选择一个要寻找的物品
-        items_to_find = ['水果', '甜品', '蔬菜']
+        # 【修改点1】定义要发送的英文标识符
+        items_to_find = ['fruits', 'vegetables', 'desserts']
         chosen_item = random.choice(items_to_find)
         
-        rospy.loginfo('[小车端] 准备请求电脑开始仿真，寻找目标: \'%s\'' % chosen_item)
+        # 打印日志时，我们可以把它翻译成中文，方便人类阅读
+        log_display_names = {'fruits': '水果', 'vegetables': '蔬菜', 'desserts': '甜品'}
+        rospy.loginfo('[小车端] 准备请求电脑开始仿真，寻找目标: %s (发送: %s)' % (log_display_names.get(chosen_item, '未知'), chosen_item))
         
-        # 调用服务，并传递请求参数。
+        # 【修改点2】发送的请求中，只包含纯英文标识符
         request = FindItemRequest(item_to_find=chosen_item)
         response = find_item_service(request)
         
         # 打印从电脑返回的响应结果
         rospy.loginfo('------------------------------------------')
         rospy.loginfo('[小车端] 收到电脑的响应！')
-        rospy.loginfo('         目标 \'%s\' 位于: %s' % (chosen_item, response.room_location))
+        # 【修改点3】收到的也是纯英文标识符，打印时可以翻译
+        rospy.loginfo('         目标 \'%s\' 位于: %s' % (log_display_names.get(chosen_item, '未知'), response.room_location))
         rospy.loginfo('------------------------------------------')
         
-    except rospy.ServiceException, e:  # Python 2.7 style exception syntax
+    except rospy.ServiceException, e:
         rospy.logerr('[小车端] 服务调用失败: %s' % e)
 
 if __name__ == '__main__':
-    # 初始化ROS节点
     rospy.init_node('simulation_client_node')
     call_simulation_service()
